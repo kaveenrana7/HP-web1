8c162332ed09c07d8b74103411000519b80f1cd3
Udana D. Kodikara <chipseyofficial@gmail.com>
2023-09-10 02:24:42 +0530

    up to dashboard

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./index.css";
-import "./home-image.png";
+import Navbar from "../Navbar/Navbar";
 
 const Home = () => {
   return (
     <div className="label">
+      <Navbar home="active" />
       <div className="page-image">
         <img src="home-image.png" alt="PageImage" />
       </div>
