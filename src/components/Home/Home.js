f5150d52cc83a92ddd7ef198896929d6a427095e
kaveenrana7 <kaveenrana5745@gmail.com>
2023-10-07 17:35:26 +0530

    new home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,25 @@
 import React from "react";
 import "./index.css";
 import Navbar from "../Navbar/Navbar";
+import HomeContent from "./HomeContent";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="label">
       <Navbar home="active" back_page="text-wrappr-home" />
-      <div className="page-image">
-        <img src="home-image.png" alt="PageImage" />
-      </div>
-      <div className="text-wrapper">YOUR LINK TO</div>
-      <div className="text-wrapper2">EXPERT SOLUTION</div>
-      <div className="text-wrapper3">HIREPRO</div>
-      <div className="label2">
-        <div className="text-wrapper4">Experience the Difference with</div>
-        <div className="text-wrapper4">HirePro's Trusted Experts</div>
-      </div>
-      <div className="download">
-        <div className="text-wrapper5">Download App</div>
+      <div className="fade-in">
+        <div className="page-image">
+          <img src="home-image.png" alt="PageImage" />
+        </div>
+        <div>
+          <HomeContent />
+        </div>
+        <Link to="/download">
+          <div className="download">
+            <div className="text-wrapper5">Download App</div>
+          </div>
+        </Link>
       </div>
     </div>
   );
